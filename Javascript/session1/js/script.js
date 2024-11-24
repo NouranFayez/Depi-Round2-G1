1f473e9c241ef4ac36bd2203771bda3f8a1eac57
NouranFayez <nouran_ahmed269@hotmail.com>
2024-11-24 21:06:54 +0200

    session 1 js

---
+++ Javascript/session1/js/script.js
@@ -0,0 +1,106 @@
+
+
+console.log('Javascript');
+
+
+// var variableName = variableValue 
+
+var firstName = "Nouran"
+
+// console.log(firstName)
+
+var age = 30
+
+// console.log(age)
+
+// boolean ==> true / false
+
+var isGraduated = true
+// console.log(isGraduated)
+
+var address
+
+// console.log(officeAddress) // error
+
+
+var r = null
+
+var lastName = 'Fayez'
+
+var middleName = "Ahmed"
+
+// + ==> concatenate
+// var fullName = firstName + " " + middleName + " " + lastName 
+
+// template string
+var fullName = `${firstName} ${middleName} ${lastName}`
+
+var fullName = "string"
+
+
+// console.log(fullName)
+
+var x = 3
+var y = 5
+var sum = x + y
+
+// console.log(sum)
+
+// console.log(x + y)
+// console.log(y - x)
+// console.log(y * x)
+
+
+
+// ++ increment 
+var h = 30
+// console.log(h++)
+// console.log(++h)
+// console.log(++h)
+
+// console.log(h)
+
+// decrement --
+// console.log(h--)
+// console.log(h)
+// console.log(--h)
+
+var num1 = "text"
+var num2 = num1 / 5
+
+// console.log(num2)
+
+
+/// Assignment operator
+
+var num = 5
+// num = num + 4
+
+// console.log(num+=4)
+// console.log(num-=2)
+// console.log(num*=2)
+
+
+// Comparison Operator : > , >= , < , <= , == , === , != , !==
+
+var n = 3 
+var m = 5
+var l = 5
+
+console.log(n > m)
+console.log(m >= l)
+
+var t = 30 // number
+var v = "30" // string
+
+console.log(t == v) // value = true
+console.log(t === v) // value - data type = false
+
+console.log(firstName == 'nouran')
+console.log("---------------------------------------")
+
+console.log(t != v) // value = false
+console.log(t !== v) // value - data type = true
+
+
+console.log(t != firstName)
