1962842bfc41c9e8f010ef17a17b7ec9fbdee672
NouranFayez <nouran_ahmed269@hotmail.com>
2025-01-04 13:03:16 +0200

    angular session 1

--- Angular/session1/src/app/app.component.ts
+++ Angular/session1/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-custom',
   templateUrl: './app.component.html',
   standalone: false,
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'session1';
 }
--- Angular/session1/src/app/app.module.ts
+++ Angular/session1/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    BlogsComponent,
+    NavbarComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
